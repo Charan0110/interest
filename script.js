782955adbba6b0fefc1c62a7c3de8a89eaf97038
Charan0110 <shivacharanvadithya@gmail.com>
2024-08-25 15:34:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,115 +1,304 @@
-const alertElement = document.querySelector(".alert");
-const passwordElement = document.querySelector(".password");
-const copyBtnElement = document.querySelector(".copy-btn");
-const charLengthElement = document.querySelector(".char-length");
-const charInputElement = document.querySelector(".slider-range");
-const numbersSwitchElement = document.querySelector(
-  'input[name="numbers-switch"]'
-);
-const symbolsSwitchElement = document.querySelector(
-  'input[name="symbols-switch"]'
-);
-const uppercaseSwitchElement = document.querySelector(
-  'input[name="uppercase-switch"]'
-);
-const submitBtnElement = document.querySelector(".submit-btn");
+const SI_select = document.getElementById("SI");
+const CI_select = document.getElementById("CI");
+let amount ;
+var RoI;
+var time;
+var loanTakenDate;
+var loanTakenDate_date;
+var loanTakenDate_month;
+var loanTakenDate_year;
+var loanRepaidDate;
+var loanRepaidDate_date;
+var loanRepaidDate_month;
+var loanRepaidDate_year;
+var interestAmount_title; 
+var totalAmount_title;
+var interestPerMonth;
+var interestAmount;
+var totalAmount;
 
-copyBtnElement.addEventListener("click", async function (e) {
-  try {
-    await copyToClipboard(passwordElement.textContent);
-    alertElement.style.visibility = "visible";
-    setTimeout(function () {
-      alertElement.style.visibility = "hidden";
-    }, 3000);
-  } catch (err) {
-    console.error("Failed to copy: ", err);
-  }
-});
+document.getElementById("simpleInterest").onclick = function(event){
+    event.preventDefault();
+    SI_select.checked = true;
 
-charInputElement.addEventListener("input", function (e) {
-  charLengthElement.textContent = e.target.value;
-});
-
-submitBtnElement.addEventListener("click", function (e) {
-  e.preventDefault();
-  const charLength = parseInt(charInputElement.value);
-  const includeNumbers = numbersSwitchElement.checked;
-  const includeSymbols = symbolsSwitchElement.checked;
-  const includeUppercase = uppercaseSwitchElement.checked;
-
-  const password = generateRandomPassword(
-    charLength,
-    includeNumbers,
-    includeSymbols,
-    includeUppercase
-  );
-  setFontSize(password);
-  passwordElement.textContent = password;
-});
-
-async function copyToClipboard(str) {
-  if (navigator.clipboard && navigator.clipboard.writeText) {
-    await navigator.clipboard.writeText(str);
-  } else {
-    throw new Error("Clipboard not supported");
-  }
+    // Set the background color of the clicked button to aqua
+    this.style.backgroundColor = "aqua";
+    
+    // Uncheck the previously selected button if any
+    document.getElementById("compoundInterest").style.backgroundColor = "";
+    
+    const months_input = document.getElementById("timeType");
+    months_input.innerText = "No. of Months";
 }
 
-function replaceRandomChar(password, replacementChar) {
-  const randomIndex = Math.floor(Math.random() * password.length);
-  const passwordArray = password.split("");
-  passwordArray[randomIndex] = replacementChar;
-  return passwordArray.join("");
+document.getElementById("compoundInterest").onclick = function(event){
+    event.preventDefault();
+    CI_select.checked = true;
+
+    // Set the background color of the clicked button to aqua
+    this.style.backgroundColor = "aqua";
+    
+    // Uncheck the previously selected button if any
+    document.getElementById("simpleInterest").style.backgroundColor = "";
+    
+    const years_input = document.getElementById("timeType");
+    years_input.innerText = "No. of Years";
 }
 
-function generateRandomPassword(
-  length,
-  includeNumbers,
-  includeSymbols,
-  includeUppercase
-) {
-  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const numberChars = "0123456789";
-  const symbolChars = "!?@#$%^&*()_";
 
-  let charset = lowercaseChars;
-  if (includeUppercase) charset += uppercaseChars;
-  if (includeNumbers) charset += numberChars;
-  if (includeSymbols) charset += symbolChars;
+//clear warning for time
+function clearWarning() {
+    document.getElementById("warning").innerText = "";
+}
+const elementsToWatch = ["Months", "loanTakenDate", "loanRepaidDate"];
+elementsToWatch.forEach(id => {
+    document.getElementById(id).onclick = clearWarning;
+});
 
-  let password = "";
 
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset[randomIndex];
-  }
+//date input
+function dateInput_check(loanTakenDate,loanRepaidDate){ 
 
-  if (includeNumbers && !/\d/.test(password)) {
-    const randomNumIndex = Math.floor(Math.random() * numberChars.length);
-    password = replaceRandomChar(password, numberChars[randomNumIndex]);
-  }
+    let loanTakenDate_validity = document.getElementById("loanTakenDate_validity");
+    let loanRepaidDate_validity = document.getElementById("loanRepaidDate_validity");   
+    
+    // Clear previous error messages
+    loanTakenDate_validity.innerText = "";
+    loanRepaidDate_validity.innerText = "";
+    
+    if(loanTakenDate && loanRepaidDate){ 
+        
+        const loanTakenDate_arr = loanTakenDate.split('-');
+        loanTakenDate_year = parseInt(loanTakenDate_arr[0]);
+        loanTakenDate_month = parseInt(loanTakenDate_arr[1]);
+        loanTakenDate_date = parseInt(loanTakenDate_arr[2]);
 
-  if (includeSymbols && !/[!?@#$%^&*()_]/.test(password)) {
-    const randomSymbolIndex = Math.floor(Math.random() * symbolChars.length);
-    password = replaceRandomChar(password, symbolChars[randomSymbolIndex]);
-  }
+        const loanRepaidDate_arr = loanRepaidDate.split('-');
+        loanRepaidDate_year = parseInt(loanRepaidDate_arr[0]);
+        loanRepaidDate_month = parseInt(loanRepaidDate_arr[1]);
+        loanRepaidDate_date = parseInt(loanRepaidDate_arr[2]);
 
-  if (includeUppercase && !/[A-Z]/.test(password)) {
-    const randomUpperIndex = Math.floor(Math.random() * uppercaseChars.length);
-    password = replaceRandomChar(password, uppercaseChars[randomUpperIndex]);
-  }
+        const loanTakenDate_obj = new Date(loanTakenDate);
+        const loanRepaidDate_obj = new Date(loanRepaidDate);
 
-  return password;
+        if(loanRepaidDate_obj < loanTakenDate_obj) {
+            loanRepaidDate_validity.innerText = "Loan repayment cannot be done befor it's taken";
+            return false;
+        }
+        return true;
+    }
+    else if(loanTakenDate && !loanRepaidDate){
+        loanRepaidDate_validity.innerText = "Please enter the Loan Repaid Date";
+        return false;
+    }
+    else if(!loanTakenDate && loanRepaidDate){
+        loanTakenDate_validity.innerText = "Please enter the Loan taken Date";
+        return false;
+    }
+    else{
+        return false;
+    }
 }
 
-function setFontSize(password) {
-  const passwordLength = password.length;
-  if (passwordLength <= 10) {
-    passwordElement.style.fontSize = "2rem";
-  } else if (passwordLength <= 15) {
-    passwordElement.style.fontSize = "1.5rem";
-  } else {
-    passwordElement.style.fontSize = "1.25rem";
-  }
+
+//Days in a month
+function getDaysInMonth(month,year){
+    return new Date(year, month, 0).getDate();
+}
+
+//Interest per month
+function getPerMonthInterest(amount,RoI){
+    let perMonthInterest = (amount*RoI)/100;
+    return perMonthInterest;
+}
+
+//case1 (same year, same month, given date is less than repaid data)
+function case1(d1,d2,pI,daysInMonth){
+    var inte = ((d2-d1)*pI)/daysInMonth;
+    return inte;
+}
+
+//case2 (same year, given month is less than repaid month, given date is less than repaid data)
+function case2(d1,d2,m1,m2,pI,daysInMonth){
+    var inte = ((m2-m1)*pI) + case1(d1,d2,pI,daysInMonth);
+    return inte;
+}
+
+//case3 (same year, given month is less than repaid month, given date is greater than repaid data)
+function case3(d1,d2,m1,m2,pI,daysInMonth){
+    var inte = ((m2-m1)*pI) - case1(d2,d1,pI,daysInMonth);
+    return inte;
+}
+
+//case4 (given year is less than repaid year, given month is less than repaid month, given date is less than repaid data)
+function case4(d1,d2,m1,m2,y1,y2,pI,daysInMonth){
+    var inte = (((y2-y1)*12)*pI) + case2(d1,d2,m1,m2,pI,daysInMonth);
+    return inte;
+}
+
+//case5 (given year is less than repaid year, given month is less than repaid month, given date is greater than repaid data)
+function case5(d1,d2,m1,m2,y1,y2,pI,daysInMonth){
+    var inte = (((y2-y1)*12)*pI) + case3(d1,d2,m1,m2,pI,daysInMonth);
+    return inte;
+}
+
+//case6 (given year is less than repaid year, given month is greater than repaid month, given date is greater than repaid data)
+function case6(d1,d2,m1,m2,y1,y2,pI,daysInMonth){
+    var inte = (((y2-y1)*12)*pI) - case3(d2,d1,m2,m1,pI,daysInMonth);
+    return inte;
+}
+
+//case7 (given year is less than repaid year, given month is greater than repaid month, given date is less than repaid data)
+function case7(d1,d2,m1,m2,y1,y2,pI,daysInMonth){
+    var inte = (((y2-y1)*12)*pI) - case2(d2,d1,m2,m1,pI,daysInMonth);
+    return inte;
+}
+
+
+//Interest Cal
+function getInterestAmount(interestPerMonth){
+    let d1 = loanTakenDate_date;
+    let d2 = loanRepaidDate_date;
+
+    let m1 = loanTakenDate_month;
+    let m2 = loanRepaidDate_month;
+
+    let y1 = loanTakenDate_year;
+    let y2= loanRepaidDate_year;
+
+    var pI = interestPerMonth;
+
+    var daysInMonth = getDaysInMonth(m2,y2);
+
+    if(y1==y2){
+        if(m1==m2){
+            return case1(d1,d2,pI,daysInMonth);
+        }
+        else{
+            if(d1<=d2){
+                return case2(d1,d2,m1,m2,pI,daysInMonth);
+            }
+            else if(d1>d2){
+                return case3(d1,d2,m1,m2,pI,daysInMonth);
+            }
+        }
+    }
+    else{
+        if(m1<=m2){
+            if(d1<=d2){
+                return case4(d1,d2,m1,m2,y1,y2,pI,daysInMonth);
+            }
+            else{
+                return case5(d1,d2,m1,m2,y1,y2,pI,daysInMonth);
+            }
+        }
+        else{
+            if(d1<=d2){
+                return case6(d1,d2,m1,m2,y1,y2,pI,daysInMonth);
+            }
+            else{
+                return case7(d1,d2,m1,m2,y1,y2,pI,daysInMonth);
+            }
+        }
+    }
+}
+
+//CI 
+function CI_getTotalAmount(amount,RoI){
+
+    let d1 = loanTakenDate_date;
+    let d2 = loanRepaidDate_date;
+
+    let m1 = loanTakenDate_month;
+    let m2 = loanRepaidDate_month;
+
+    let y1 = loanTakenDate_year;
+    let y2= loanRepaidDate_year;
+
+    var pI = getPerMonthInterest(amount,RoI);
+
+    while(y1!=y2){
+        amount += (12*pI);
+        y1++;
+        if(y1==y2) break;
+        pI = getPerMonthInterest(amount,RoI);
+    }
+
+    var daysInMonth = getDaysInMonth(m2,y2);
+    if(m1<=m2){
+        pI = getPerMonthInterest(amount,RoI);
+        if(d1<=d2){
+            amount += case2(d1,d2,m1,m2,pI,daysInMonth);
+            return amount;
+        }
+        else{
+            amount += case3(d1,d2,m1,m2,pI,daysInMonth);
+            return amount;
+        }
+    }
+    else if(m1>m2){
+        if(d1<=d2){
+            amount -= case3(d2,d1,m2,m1,pI,daysInMonth);
+            return amount;
+        }
+        else{
+            amount -= case2(d2,d1,m2,m1,pI,daysInMonth);
+            return amount;
+        }
+    }
+    
+}
+
+
+//after clicking submit
+document.getElementById("calculate").onclick = function(event){
+    event.preventDefault();
+
+    const amount_input = document.getElementById("Amount");
+    const RoI_input = document.getElementById("RoI");
+
+    amount = parseFloat(amount_input.value);
+    RoI = parseFloat(RoI_input.value);
+
+    const time_input = document.getElementById("Months");
+
+    time = parseFloat(time_input.value);
+
+    const loanTakenDate_input = document.getElementById("loanTakenDate");
+    const loanRepaidDate_input = document.getElementById("loanRepaidDate");
+
+    loanTakenDate = loanTakenDate_input.value;
+    loanRepaidDate = loanRepaidDate_input.value;
+
+    interestAmount_title = document.getElementById("interestAmount");
+    totalAmount_title = document.getElementById("totalAmount");
+
+    if(CI_select.checked == true){
+        if(dateInput_check(loanTakenDate,loanRepaidDate)==true && !time){
+            totalAmount = CI_getTotalAmount(amount,RoI); 
+        }
+        else if(time){
+            totalAmount = amount*(Math.pow((1+(RoI/(100*12))),(12*time)));
+        }
+        else{
+            document.getElementById("warning").innerText = "Please enter any one of the below fields";
+        }
+        interestAmount = totalAmount - amount;
+    }
+
+    else{
+        if(dateInput_check(loanTakenDate,loanRepaidDate)==true && !time){
+            interestPerMonth = getPerMonthInterest(amount,RoI);
+            interestAmount = getInterestAmount(interestPerMonth);
+        }
+        else if(time){
+            interestAmount = (amount*RoI*time)/100;
+        }
+        else{
+            document.getElementById("warning").innerText = "Please enter any one of the below fields";
+        }
+        totalAmount = amount + interestAmount;
+    }
+    interestAmount_title.textContent = `Interest Amount: ${interestAmount.toFixed(2)}`;
+    totalAmount_title.textContent = `Total Amount: ${totalAmount.toFixed(2)}`;
 }
